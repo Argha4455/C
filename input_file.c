378bff6bf50e6eeb2c61c819bb44b37b00fd6e1a
argha <chakrabartyargha31@gmail.com>
2024-08-18 15:18:05 +0530

    modifying two files

--- input_file.c
+++ input_file.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+   void main()
+   {
+      FILE *fp;
+      char letter;
+      int letter_len=0;
+      int space=0;
+      int newline=0;
+      fp=fopen("argha.txt","r");
+      do{
+        letter=fgetc(fp);
+        if(letter==EOF)
+            break;
+        letter_len++;
+        if(letter==' ')
+            space++;
+        if(letter=='\n')
+            newline++;
+      }while(1);
+      printf("the newline-%d,the spaces-%d,the total charecters-%d",newline,space,letter_len);
+      fclose(fp);
+   }
